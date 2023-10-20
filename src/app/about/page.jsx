73164fb1f3007ac0ad9e8f3059aa8aba40e448c0
Disha <dishapatil0517@gmail.com>
2023-10-20 14:27:21 +0530

    It's about the 'About Page'

---
+++ src/app/about/page.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+export default function page() {
+  return (
+    <div className="max-w-6xl mx-auto space-y-4">
+        <h1 className="text-2xl font-medium text-amber-600">About</h1>
+        <p>
+        Welcome to our awesome movie hub! We are a bunch of movie buffs who have teamed up to bring you a one-stop destination for all your movie cravings. Our website is your go-to place for a treasure trove of movies from across the globe. From the hottest news to the coolest trailers, we have got you covered. We update our database regularly, so you never miss out on the latest flicks.                 
+        </p>                                                                                                                                                                                                 
+        <p>
+        To make your viewing experience even better, we've included a switchable dark and light mode for your comfort. No more straining your eyes during those late-night movie marathons!
+        </p>                                                                                                                                                                                                  
+        <p>
+        Besides our amazing movie collection, we've got a place for you to hang out with fellow movie lovers. Our community corner lets you chat about your favorite movies, read reviews, and find new gems. Stay in the loop with our latest movie news and trailers, keeping you on top of all things cinema. we hope you have a blast exploring our movie wonderland.
+        Happy movie hunting!
+        </p>
+    </div>
+  )
+}
