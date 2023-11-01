edbc46703f5dab7e5988ed3f8910dafda23f08e0
Disha <dishapatil0517@gmail.com>
2023-11-01 15:55:51 +0530

    Facing Error whiling deploying

--- src/app/loading.jsx
+++ src/app/loading.jsx
@@ -1,7 +1,14 @@
+import Image from "next/image";
+
 export default function loading() {
   return (
     <div className="flex justify-center">
-        <img className="h-26" src="Spinner.svg" alt="loading..." />
+        <Image 
+          className="h-26" 
+          src="Spinner.svg" 
+          alt="loading..." 
+          width={100} 
+          height={100}/>
     </div>
   )
 }
